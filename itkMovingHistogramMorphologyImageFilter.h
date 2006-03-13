9327d04c624b4c206b907953cd194cac1181f4be
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-13 17:06:44 +0100

    create a specialized version for small integer types
    darcs-hash:20060313160644-2fc9d-66b6a18c36dcf57bf82fd0f8e4a0cfe7e2d68fd1.gz

--- itkMovingHistogramMorphologyImageFilter.h
+++ itkMovingHistogramMorphologyImageFilter.h
@@ -29,34 +29,94 @@ template <class TInputPixel, class TCompare>
 class MorphologyHistogram
 {
 public:
-  MorphologyHistogram(){}
+  MorphologyHistogram()
+    {
+    if( useVectorBasedAlgorithm() )
+      { initVector(); }
+    }
   ~MorphologyHistogram(){}
 
-  typedef typename std::map< TInputPixel, unsigned long, TCompare > HistogramType;
 
-  inline TInputPixel operator()( const HistogramType &histogram )
-  {
-    if( !histogram.empty() )
-      { return histogram.rbegin()->first - histogram.begin()->first; }
-    return 0;
-  }
+
+  // define the method required by the functor and dispatch to the specialized methods
+
 
   inline void AddBoundary()
-    { m_Map[ m_Boundary ]++; }
+    {
+    if( useVectorBasedAlgorithm() )
+      { AddBoundaryVector(); }
+    else
+      { AddBoundaryMap(); }
+    }
 
   inline void RemoveBoundary()
-    { m_Map[ m_Boundary ]--; }
+    {
+    if( useVectorBasedAlgorithm() )
+      { RemoveBoundaryVector(); }
+    else
+      { RemoveBoundaryMap(); }
+    }
 
   inline void AddPixel( const TInputPixel &p )
-    { m_Map[ p ]++; }
+    {
+    if( useVectorBasedAlgorithm() )
+      { AddPixelVector( p ); }
+    else
+      { AddPixelMap( p ); }
+    }
 
   inline void RemovePixel( const TInputPixel &p )
-    { m_Map[ p ]--; }
+    {
+    if( useVectorBasedAlgorithm() )
+      { RemovePixelVector( p ); }
+    else
+      { RemovePixelMap( p ); }
+    }
 
   inline TInputPixel GetValue()
     {
+    if( useVectorBasedAlgorithm() )
+      { return GetValueVector(); }
+    else
+      { return GetValueMap(); }
+    }
+
+
+  inline bool useVectorBasedAlgorithm()
+    {
+    // bool, short and char are acceptable for vector based algorithm: they do not require
+    // too much memory. Other types are not usable with that algorithm
+    return typeid(TInputPixel) == typeid(unsigned char)
+        || typeid(TInputPixel) == typeid(signed char)
+        || typeid(TInputPixel) == typeid(unsigned short)
+        || typeid(TInputPixel) == typeid(signed short)
+        || typeid(TInputPixel) == typeid(bool);
+    }
+
+
+
+  //
+  // the map based algorithm
+  //
+
+  typedef typename std::map< TInputPixel, unsigned long, TCompare > MapType;
+
+  inline void AddBoundaryMap()
+    { m_Map[ m_Boundary ]++; }
+
+  inline void RemoveBoundaryMap()
+    { m_Map[ m_Boundary ]--; }
+
+  inline void AddPixelMap( const TInputPixel &p )
+    { m_Map[ p ]++; }
+
+  inline void RemovePixelMap( const TInputPixel &p )
+    { m_Map[ p ]--; }
+
+  inline TInputPixel GetValueMap()
+    {
     // clean the map
-    typename HistogramType::iterator mapIt = m_Map.begin();
+    typename MapType::iterator mapIt = m_Map.begin();
     while( mapIt != m_Map.end() )
       {
       if( mapIt->second == 0 )
@@ -81,10 +141,66 @@ public:
     return m_Map.begin()->first;
     }
 
+  MapType m_Map;
+
+
+
+
+  //
+  // the vector based algorithm
+  //
+
+  inline void initVector()
+    {
+    // initialize members need for the vector based algorithm
+    m_Vector.resize( static_cast<int>( NumericTraits< TInputPixel >::max() - NumericTraits< TInputPixel >::NonpositiveMin() + 1 ), 0 );
+    if( m_Compare( NumericTraits< TInputPixel >::max(), NumericTraits< TInputPixel >::NonpositiveMin() ) )
+      {
+      m_CurrentValue = NumericTraits< TInputPixel >::NonpositiveMin();
+      m_Direction = -1;
+      }
+    else
+      {
+      m_CurrentValue = NumericTraits< TInputPixel >::max();
+      m_Direction = 1;
+      }
+    }
+  
+
+  inline void AddBoundaryVector()
+    { AddPixelVector( m_Boundary ); }
+
+  inline void RemoveBoundaryVector()
+    { RemovePixelVector( m_Boundary ); }
+
+  inline void AddPixelVector( const TInputPixel &p )
+    {
+    m_Vector[ static_cast<int>( p - NumericTraits< TInputPixel >::NonpositiveMin() ) ]++;
+    if( m_Compare( p, m_CurrentValue ) )
+      { m_CurrentValue = p; }
+    }
+
+  inline void RemovePixelVector( const TInputPixel &p )
+    {
+    m_Vector[ static_cast<int>( p - NumericTraits< TInputPixel >::NonpositiveMin() ) ]--;
+    while( m_Vector[ static_cast<int>( m_CurrentValue - NumericTraits< TInputPixel >::NonpositiveMin() ) ] == 0 )
+      { m_CurrentValue += m_Direction; }
+    }
+
+  inline TInputPixel GetValueVector()
+    { return m_CurrentValue; }
+
+  std::vector<unsigned long> m_Vector;
+  TInputPixel m_CurrentValue;
+  TCompare m_Compare;
+  signed int m_Direction;
+
+
+
+  // accessor for boundary value
+
   void SetBoundary( const TInputPixel & val )
     { m_Boundary = val; }
-
-  HistogramType m_Map;
 
   TInputPixel m_Boundary;
 };
